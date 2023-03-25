304685ea00ac4a829c024002b32677280e068ce0
Sam Thorold <sam.thorold@gmail.com>
2023-03-25 14:41:53 +0000

    Tests setup

--- models/card.py
+++ models/card.py
@@ -6,6 +6,21 @@ from models.enums import Faction, Type
 
 @dataclass
 class Card:
+    """
+    Examples:
+    >>> from models.enums import Faction, Type
+    >>> Card(
+    ...     name="scout",
+    ...     type=Type.SHIP,
+    ...     faction=Faction.NONE,
+    ...     cost=0,
+    ...     money=1,
+    ...     actions=[],
+    ... )
+    Card(name='scout', type=<Type.SHIP: 2>, faction=<Faction.NONE: 1>, cost=0, actions=[], money=1)
+
+    """
+
     name: str
     type: Type
     faction: Faction
