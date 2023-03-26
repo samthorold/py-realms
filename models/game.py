c0b2cb52c7b6c06009722fe4d815be42d603cdf8
Sam Thorold <sam.thorold@gmail.com>
2023-03-26 12:46:13 +0100

    Starting hand init

--- models/game.py
+++ models/game.py
@@ -1,6 +1,15 @@
+from models.card import Card
 from models.deck import Deck
 from models.player import Player
 
+
+# Stronger / more useful typedef would be 3 / 5 scouts or vipers
+Player1StartingHand = tuple[Card, Card, Card]
+Player2StartingHand = tuple[Card, Card, Card, Card, Card]
+
+
+def starting_hands(player_starting_deck: list[Card]) -> tuple[Player1StartingHand, Player2StartingHand]:
+    """Create the starting hands for each player."""
 
 class Game:
     """Orchestrate the Deck and Players."""
