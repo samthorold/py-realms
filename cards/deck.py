58899c57c1928ef8ca2de7990f3a6cee59edf796
Sam Thorold <sam.thorold@gmail.com>
2023-03-26 15:39:43 +0100

    Import cards from faction module

--- cards/deck.py
+++ cards/deck.py
@@ -1,10 +1,8 @@
-from cards.factionless import SCOUT
-from cards.factionless import VIPER
-from cards.federation import CUTTER
-from cards.federation import FEDERATION_SHUTTLE
+from cards.factionless import SCOUT, VIPER
+from cards.federation import CUTTER, FEDERATION_SHUTTLE
 
 
-PLAYER_STARTING_DECK = tuple([VIPER] * 2 + [SCOUT] * 5)
+PLAYER_STARTING_DECK = tuple([VIPER] * 2 + [SCOUT] * 8)
 
 
 GAME_STARTING_DECK = tuple([CUTTER] * 4 + [FEDERATION_SHUTTLE] * 4)
