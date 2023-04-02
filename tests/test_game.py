a84e2fcf504381794979e6eedff7b96e552fec73
Sam Thorold <sam.thorold@gmail.com>
2023-04-02 11:05:55 +0100

    #5 Actions increased by card actions.

--- tests/test_game.py
+++ tests/test_game.py
@@ -11,10 +11,27 @@ def test_game_init() -> None:
 def test_play() -> None:
     game = Game()
     game.action("START_GAME")
-    game.action("play", idx=0)
     pl = game.get_current_player()
-    assert len(pl._hand) < 5
-    assert len(pl._in_play) > 0
+
+    # grab the player's hand before the action
+    og_hand = list(pl._hand)
+    og_in_play = list(pl._in_play)
+
+    # get the card they are about to play
+    card = pl._hand[0]
+
+    # get the count of that card pre-action
+    og_hand_count = len([c for c in og_hand if c == card])
+    og_in_play_count = len([c for c in og_in_play if c == card])
+
+    game.action("play", idx=0)
+
+    new_hand_count = len([c for c in pl._hand if c == card])
+    new_in_play_count = len([c for c in pl._in_play if c == card])
+
+    assert len(pl._hand) < 3 and new_hand_count < og_hand_count, card
+    assert len(pl._in_play) > 0 and new_in_play_count > og_in_play_count, card
+    assert all(c in game._actions for c in card.actions), card.actions
 
 
 def test_unknown_action() -> None:
